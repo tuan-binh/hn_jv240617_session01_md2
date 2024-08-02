e96f4cf618513671160d286615ef496a605eadb7
= <=>
2024-08-02 14:55:36 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
 //        System.out.println("Nhâp số");
 //        float number = Float.parseFloat(sc.nextLine());
 //        System.out.println("Nhập chuỗi");
@@ -13,17 +19,22 @@ public class Main {
 
         // in
         // System.out.println() // line new
-        System.out.println("1");
-        System.out.println("2");
-        // System.out.print() // giống inline
-        System.out.print("3");
-        System.out.print("4");
+//        System.out.println("1");
+//        System.out.println("2");
+//        // System.out.print() // giống inline
+//        System.out.print("3");
+//        System.out.print("4");
         // System.out.printf() // format
         /**
          * %s - String
          * %d - số nguyên
          * %f - số thực
          * */
-        System.out.printf("[ %10d | %10s | %10f ]",1,"1331231",3.2);
+        String text = "abcd";
+        double a = 3.25633321;
+        System.out.printf("| %d | %s | %.2f |\n", 1, text, a);
+        System.out.printf("| %-10d | %-10s | %-10f |\n", 1, StringUtils.center(text, 10), 3.2);
+        System.out.printf("| %-10d | %-10s | %-10f |\n", 1, text, 3.2);
+
     }
 }
